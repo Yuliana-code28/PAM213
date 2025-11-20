3796e551e5db2650db0fb980f2822018e67ace76
Yuliana-code28 <124048858@upq.edu.mx>
2025-11-19 20:24:53 -0600

    Bottom Tabs Navigator

---
+++ tabsNavigation/screens/settings.js
@@ -0,0 +1,34 @@
+import {View, Text, StyleSheet} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
+
+export default function Settings(){
+    return(
+        <View style={styles.container}>
+            <View style={styles.iconRow}>
+                <Ionicons name="settings-outline" size={28} color="blue"/>
+                <Text style={styles.title}>Configuraciones de usuario</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        padding: 20,
+        justifyContent: 'center', // Centra verticalmente
+        alignItems: 'center',
+    },
+    iconRow:{
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    title:{
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginLeft: 10,
+        color: 'blue',
+    },
+});
